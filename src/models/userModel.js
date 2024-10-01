4da3d788b4ccfeb9c67e398cb9cf0eba0fa47b4a
mostafa2113 <ace13520@gmail.com>
2024-10-01 22:08:01 +0300

    setting db models

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true
+    },
+    email: {
+      type: String,
+      match: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+      required: [true, "Please provide an email"],
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6
+    },
+    avatar: {
+      type: String,
+      default: ""
+    },
+    phone: {
+      required: [true, "Please provide a phone number"],
+      type: String,
+      default: ""
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
