7df0a694481e7250e8a7953e4e6d4d2e27567fa7
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2024-10-18 07:01:01 +0300

    Make change Password
    Make change Password

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,7 +8,8 @@ const {
   resetPassword,
   verifyEmail,
   logout,
-  updateUser
+  updateUser,
+  updatePassword
 } = require("../controllers/userController");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
@@ -24,6 +25,9 @@ router.post("/forgot-password", forgotPasswordValidator, forgotPassword);
 router.post("/verify", verifyEmail);
 router.post("/reset-password/:resetToken", resetPassword);
 router.get("/logout", authMiddleware, logout);
+
+router.put("/change-password", authMiddleware, updatePassword);
+
 
 router
   .route("/")
